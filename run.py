c6b3989069c74b8cb50f08271f9b1cbaf460d9c9
VladlmlrF <vladimirfalin84@gmail.com>
2023-06-14 11:21:42 +0700

    initial commit

---
+++ run.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('index.html', title='Home')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
